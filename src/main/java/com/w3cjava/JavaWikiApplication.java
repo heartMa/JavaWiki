ab708fa04a5e429528f578495665b77b463eec0d
2zheteng <netheartma@gmail.com>
2019-07-31 14:35:45 +0800

    去除scan

--- src/main/java/com/w3cjava/JavaWikiApplication.java
+++ src/main/java/com/w3cjava/JavaWikiApplication.java
@@ -1,12 +1,10 @@
 package com.w3cjava;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@MapperScan("com.w3cjava.modules.*.mapper")
 @EnableTransactionManagement
 public class JavaWikiApplication {
 	public static void main(String[] args) {
